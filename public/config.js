552c350e961e2ca5edd7657ad65ae6b53798c9ae
Mario Reder <mario.reder@pm.me>
2026-02-10 13:37:59 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Leaderboard,Trading,Portfolio,Markets,Swap,Points",
-  "VITE_CUSTOM_MENUS": "Trading Competition, https://trading.fun/competitions/8c22890e-ce0b-4353-b0ce-2fc90e22954b",
+  "VITE_CUSTOM_MENUS": "Buy Crypto, https://www.onramper.com/buy",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://www.google.com/?hl=es",
